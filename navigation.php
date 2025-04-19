7cf6d0e1c1e4fd252b34f09f5358c8e58ec2a4a2
Ujjwal-0005 <151321517+Ujjwal-0005@users.noreply.github.com>
2025-04-19 17:52:55 +0530

    Update navigation.php
    changed complete

--- navigation.php
+++ navigation.php
@@ -1,14 +1,23 @@
+<style>
+    .hh{
+        background-color: #7f5a83;
+background-image: linear-gradient(315deg, #7f5a83 0%, #0d324d 74%);
+
+
+}
+    </style>
+
 <nav class="container mx-auto px-6 py-4">
     <div class="flex justify-between items-center">
-        <div class="text-2xl text-white font-bold">NutriTrack<span class="text-secondary">2025</span></div>
+        <div class="text-2xl font-bold text-white">NutriTrack</div>
         <div class="space-x-4 hidden md:flex items-center">
-            <a href="index.php" class="text-secondary hover:text-secondary transition">Home</a>
+            <a href="index.php" class="text-secondary p-[8px]   hover:text-white hover:font-bold text-white transition">Home</a>
             <?php if (isset($_SESSION['firstname'])): ?>
-                <a href="dashboard/user.php" class="hover:text-secondary text-white transition">Dashboard</a>
-                <a href="settings/user.php" class="hover:text-secondary text-white transition">Profile</a>
-                <a href="logout.php" class="bg-white text-primary px-4 py-2 rounded-lg shadow hover:bg-opacity-90 transition">Sign Out</a>
+                <a href="dashboard/user.php" class="hover:text-white text-white hover:font-bold  transition">Dashboard</a>
+                <a href="settings/user.php" class="hover:text-white text-white hover:font-bold  transition">Profile</a>
+                <a href="logout.php" class=" hh bg-white text-white  text-primary hover:font-bold  px-4 py-2  rounded-lg shadow hover:bg-opacity-90 button-glow transition">Sign Out</a>
             <?php else: ?>
-                <a href="login.php" class="bg-white text-primary px-4 py-2 rounded-lg shadow hover:bg-opacity-90 transition">Login</a>
+                <a href="login.php" class="hh bg-white text-white  text-primary hover:font-bold  px-4 py-2  rounded-lg shadow hover:bg-opacity-90 button-glow transition">Login</a>
             <?php endif; ?>
         </div>
         <button class="md:hidden text-2xl"><i class="fas fa-bars"></i></button>
