5af8ed8761e33a36f752bf64d0b9cb6cf85169fd
Shashank Pandey <jpshashank200@gmail.com>
2025-04-20 21:31:27 +0530

    Updated nav_dashboard

--- nav_dashboard.php
+++ nav_dashboard.php
@@ -13,13 +13,12 @@
             <a href="../index.php" class="hover:text-secondary transition">Home</a>
             <?php if (isset($_SESSION['firstname'])): ?>
                 <a href="../dashboard/user.php" class="hover:text-secondary transition">Dashboard</a>
-                <a href="challenges.php" class="py-2 px-3 rounded-md">Challenges</a>
+                <a href="../dashboard/challenges.php" class="py-2 px-3 rounded-md">Challenges</a>
                 <a href="../settings/user.php" class="hover:text-secondary transition">Profile</a>
                 <?php if ($_SESSION['admin'] == 1): ?>
                     <a href="../admin/dashboard.php" class="hover:text-secondary transition">Admin Dashboard</a>
                 <?php endif; ?>
                 <a href="../logout.php" class="bg-white text-white text-primary hhhh px-4 py-2 rounded-lg shadow hover:bg-opacity-90 transition animate-pulse">Sign Out</a>
-                
             <?php else: ?>
                 <a href="../login.php" class="bg-white text-white button-glow hover-scale text-primary hhhh px-4 py-2 rounded-lg shadow hover:bg-opacity-90 transition">Login</a>
             <?php endif; ?>
